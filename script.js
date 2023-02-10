2ccf40249ee7f10c2b9d9b3eb9dde7bd4cda8b85
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-10 19:55:56 +0800

    feat: add password mismatch message logic

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const passwordField = document.querySelector('#password');
 const passwordConfirmField = document.querySelector('#password-confirm');
+const passwordWarningText = document.querySelector('.sign-up__content__form__fields__password-warning');
 
 // if the values of the password fields are not the same, set a custom attribute "isvalid" to false, and vice-versa
 passwordField.addEventListener('keyup', () => {
     if (passwordField.value != passwordConfirmField.value) {
         passwordField.setAttribute('isvalid', 'false');
         passwordConfirmField.setAttribute('isvalid', 'false');
+        passwordWarningText.style.display = 'block';
     }
     else if (passwordField.value == passwordConfirmField.value) {
         passwordField.setAttribute('isvalid', 'true');
         passwordConfirmField.setAttribute('isvalid', 'true');
+        passwordWarningText.style.display = 'none';
     }
 })
 
@@ -18,9 +21,11 @@ passwordConfirmField.addEventListener('keyup', () => {
     if (passwordField.value != passwordConfirmField.value) {
         passwordField.setAttribute('isvalid', 'false');
         passwordConfirmField.setAttribute('isvalid', 'false');
+        passwordWarningText.style.display = 'block';
     }
     else if (passwordField.value == passwordConfirmField.value) {
         passwordField.setAttribute('isvalid', 'true');
         passwordConfirmField.setAttribute('isvalid', 'true');
+        passwordWarningText.style.display = 'none';
     }
 })
